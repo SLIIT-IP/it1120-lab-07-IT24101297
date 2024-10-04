750d46908956b33cd65abc38303239b0e2268212
IT24101297 <it24101297@my.sliit.lk>
2024-10-04 10:31:32 +0530

    Add files via upload

---
+++ Q2/IT24101297Lab7Q2A.java
@@ -0,0 +1,12 @@
+public class IT24101297Lab7Q2A {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+
+            for (int j = 0; j < 5; j++) {
+                System.out.print("$ ");
+            }
+
+            System.out.println();
+        }
+    }
+}
